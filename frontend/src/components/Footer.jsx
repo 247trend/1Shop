2e5b8c71cbd323f4a0bb04b6d2ca5c0245182fc5
Jason Yuan <jasonyuan0130@gmail.com>
2022-09-09 15:50:06 +0930

    Set up basic layout for navbar, content, footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,17 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            Copyright &copy; 1Shop
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  ) 
 }
 
 export default Footer
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,25 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
 const Header = () => {
   return (
-    <header>Header</header>
+    <header>
+      <Navbar bg='light' expand='lg'>
+        <Container>
+          <Navbar.Brand href='/'>1Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link href='/cart'>
+                <i className='fas fa-shopping-cart'></i> CART
+              </Nav.Link>
+              <Nav.Link href='/login'>
+                <i className='fas fa-user'></i> SIGN IN
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   )
 }
 
