b4ab6a6c093549a0c5e1bf52d99904e33dbe548a
Jason Yuan <jasonyuan0130@gmail.com>
2022-09-10 15:11:25 +0930

    Add react router to pages, navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import ProductPage from './pages/ProductPage'
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
 const App = () => {
   return (
     <>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Home />
-        </Container>
-      </main>
-      <Footer />
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/product/:id' element={<ProductPage />}/>
+          </Routes>
+          </Container>
+        </main>
+        <Footer />
+      </Router>
     </>
   )
 }
